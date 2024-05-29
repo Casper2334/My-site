87d1883b965654e0cfabe3e133ba3b8a96c22a78
Casper2334 <148195178+Casper2334@users.noreply.github.com>
2024-05-29 17:30:54 +0300

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Cats Tutorial - 3min ⏱️
           </Link>
         </div>
       </div>
